cdd7778bca56be8743dfb4e40f46aab9bc2bea04
anyi-anyi-ai <ay1839628583@outlook.com>
2026-06-04 23:06:27 +0800

    Modify next.config.ts for export output and images
    Updated Next.js configuration to export output and set images to unoptimized.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // 如果你有用到图片优化，可能还需要加上下面这行，没有用到可以不加
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
